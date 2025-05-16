09d82c92a57007045cbef70e5ed4b1c1b994135b
Marion Cts <104251209+MarionCts@users.noreply.github.com>
2025-05-16 15:08:15 +0200

    Adding JavaScript

---
+++ script.js
@@ -0,0 +1,54 @@
+// Exo 1
+
+const nom = "Jean"
+let age = 25
+let ageFutur = 5
+
+console.log(`${nom} a ${age} ans aujourd'hui, mais dans 5 ans il aura ${age + ageFutur} ans.`)
+
+// Exo 2
+
+let a = 10
+let b = 20
+let somme = a + b
+
+console.log(somme);
+
+somme = a * b
+
+console.log(somme);
+
+// Exo 3
+
+// let nombre = prompt("Entrez un nombre :")
+
+// if (nombre > 10) {
+//     console.log(`${nombre} est supérieur à 10.`);
+// } else {
+//     console.log(`${nombre} est plus petit ou égal à 10.`);
+// }
+
+// Exo 4
+
+const numero = 4
+
+if (numero % 2 === 0) {
+    console.log(`${numero} est un chiffre pair.`);
+} else {
+    console.log(`${numero} est un chiffre impair.`);
+}
+
+// Exo 5
+
+let ageUtilisateur = prompt("Quel âge as-tu ?")
+
+if (ageUtilisateur < 18) {
+    console.log(`Tu es mineur.`);
+} else if (ageUtilisateur < 65) {
+    console.log(`Tu es adulte.`);
+} else {
+    console.log(`Tu es senior.`);
+}
+
+// Exo 6
+
